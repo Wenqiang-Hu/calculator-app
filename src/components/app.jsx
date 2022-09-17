cdc8f163a951c4999eb130ac35746fbb73da6e61
Wenqiang-Hu <63572326+Wenqiang-Hu@users.noreply.github.com>
2022-09-17 16:53:10 -0700

    Building up the routes

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import NavBar from './navBar';
-
+import Calculator from './content/calculator';
+import Home from './content/home';
+import Login from './content/login';
+import NotFound from './content/NotFound';
+import Register from './content/register';
 
 class App extends Component {
     state = {  } 
@@ -8,6 +13,16 @@ class App extends Component {
         return (
             <React.Fragment>
                 <NavBar />
+                <div className='container'>
+                    <Routes>
+                        <Route path='/' element={<Home />} />
+                        <Route path='/home' element={<Home />} />
+                        <Route path='/login' element={<Login />} />
+                        <Route path='/register' element={<Register />} />
+                        <Route path='/404' element={<NotFound />} />
+                        <Route path='*' element={<Navigate replace to="/404" />} />
+                    </Routes>
+                </div>
             </React.Fragment>
         );
     }
--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class NavBar extends Component {
     state = {  } 
@@ -6,25 +7,25 @@ class NavBar extends Component {
         return (
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className='container'>
-                    <a className="navbar-brand" href="#">Online Tool</a>
+                    <Link className="navbar-brand" to={"/"}>Online Tool</Link>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarText">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item active">
-                            <a className="nav-link" href="#">Home</a>
+                            <Link className="navbar-brand" to={"/home"}>Home</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Calculator</a>
+                            <Link className="navbar-brand" to={"/calculator"}>Calculator</Link>
                         </li>
                     </ul>
                     <ul className="navbar-nav ">
                         <li className="nav-item active">
-                            <a className="nav-link" href="#">Login</a>
+                            <Link className="navbar-brand" to={"/login"}>Login</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Register</a>
+                            <Link className="navbar-brand" to={"/register"}>Register</Link>
                         </li>
                     </ul>
                     </div>
