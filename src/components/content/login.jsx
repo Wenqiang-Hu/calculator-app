ffde8100afa076e9ecfe36c62069cbdd6394e0b1
Wenqiang-Hu <63572326+Wenqiang-Hu@users.noreply.github.com>
2022-09-20 16:00:48 -0700

    building login page

--- src/components/content/login.jsx
+++ src/components/content/login.jsx
@@ -2,11 +2,42 @@ import React, { Component } from 'react';
 import Base from './base';
 
 class Login extends Component {
-    state = {  } 
+    state = {
+        error_message: "",
+        username: "",
+        password: "",
+    } 
+
+    handleClick = e => {
+        e.preventDefault();
+
+        console.log(this.state);
+    }
     render() { 
         return (
             <Base>
-                <h4>Login page</h4>
+            <div className="container">
+                <div className="row justify-content-md-center">
+                    <div className="col col-sm-3">
+                    <form>
+                    <div className="mb-3">
+                        <label htmlFor="username">Username</label>
+                        <input onChange={e => {this.setState({username: e.target.value})}} type="username" className="form-control" id="username" aria-describedby="emailHelp"/>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="password">Password</label>
+                        <input onChange={e => {this.setState({password: e.target.value})}}type="password" className=" form-control" id="password"/>
+                    </div>
+
+                    <div style={{height: "2rem", color: "red"}}>
+                        {this.state.error_message}
+                    </div>
+                    <button onClick={this.handleClick} style={{width: "100%"}} type="submit" className="btn btn-primary">Submit</button>
+                </form>
+                    </div>
+                </div>
+            </div>
+                
             </Base>
         );
     }
